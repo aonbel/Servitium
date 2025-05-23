ca37c66ec5662c155974979b27a40099485c9331
aonbel <anbel13219@gmail.com>
2025-05-24 02:20:32 +0300

    Add docker

---
+++ src/Servitium/Migrations/20250522123138_RemoveCoordinates.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Servitium.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCoordinates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Coordinates_Latitude",
+                table: "ServiceProviders");
+
+            migrationBuilder.DropColumn(
+                name: "Coordinates_Longitude",
+                table: "ServiceProviders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Coordinates_Latitude",
+                table: "ServiceProviders",
+                type: "numeric(18,4)",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Coordinates_Longitude",
+                table: "ServiceProviders",
+                type: "numeric(18,4)",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
